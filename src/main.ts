30b56a8ab59ec633e5dcb5d99420cbd053702c06
Brandon Putre <brandon.putre@gmail.com>
2020-05-07 16:47:22 -0600

    optimizes for prod deploy

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as config from 'config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const serverConfig = config.get('server');
+
+  if(process.env.NODE_ENV === 'development') {
+    app.enableCors()
+  } else {
+    app.enableCors({ origin: serverConfig.origin})
+  }
+
+
+
   app.enableCors();
-  await app.listen(3000);
+  const port = process.env.PORT || serverConfig.port;
+  await app.listen(port);
 }
 bootstrap();
